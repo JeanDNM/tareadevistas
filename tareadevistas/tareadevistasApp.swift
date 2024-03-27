ff63c6edb89ab8f9a122222fb577786ca56bac82
jean <jeanneyra06@gmail.com>
2024-03-27 12:57:27 -0700

    Initial Commit

---
+++ tareadevistas/tareadevistasApp.swift
@@ -0,0 +1,17 @@
+//
+//  tareadevistasApp.swift
+//  tareadevistas
+//
+//  Created by Jeanpierre on 27/3/24.
+//
+
+import SwiftUI
+
+@main
+struct tareadevistasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
